259e35ce13f57b94ea9ef3fd8dd81d11c3455cd4
Gaurav Sharma <gau1711sha@gmail.com>
2025-04-11 15:25:39 +0530

    project controller completed

--- src/controllers/project.controller.js
+++ src/controllers/project.controller.js
@@ -4,6 +4,7 @@ import ApiError from "../utils/ApiError.js";
 import ApiResponse from "../utils/ApiResponse.js";
 import Project  from "../models/project.model.js";
 import ProjectMember from "../models/projectmember.model.js";
+import User from '../models/user.models.js'
 
 const createProject  = asyncHandler(async(req,res)=>{
   
@@ -97,7 +98,7 @@ const updateProject = asyncHandler(async (req, res) => {
 
     const allProjects = await Project.find({});
 
-    if(allProjects,length===0){
+    if(allProjects.length===0){
         throw new ApiError(404,"no project found");
     }
 
@@ -108,4 +109,152 @@ const updateProject = asyncHandler(async (req, res) => {
 
   })
 
-export {createProject,updateProject,deleteProject,getProjectById,getProjects}
+
+
+  const addMemberToProject = asyncHandler(async(req,res)=>{
+    const { userId, role } = req.body; 
+    const { projectId } = req.params;
+    const currentUserId = req.user.id;
+
+    if(!userId){
+        throw new ApiError(400, "userId is required to add a member");
+    }
+
+const project = await Project.findById(projectId);
+
+if (!project) {
+    throw new ApiError(404, "Project not found");
+}
+
+if (project.createdBy.toString() !== currentUserId.toString()) {
+    throw new ApiError(403, "You are not authorized to add members to this project");
+}
+
+const user = await User.findById(userId);
+
+if (!user) {
+    throw new ApiError(404, "User to be added not found");
+}
+
+const existingMembership = await ProjectMember.findOne({
+    user:userId,
+    project:projectId,
+});
+
+
+if (existingMembership) {
+    throw new ApiError(409, "User is already a member of this project");
+}
+
+const newMember = await ProjectMember.create({
+    user: userId,
+    project: projectId,
+ role: role || UserRolesEnum.MEMBER,
+})
+
+return res.status(201).json(
+    new ApiResponse(201, {
+        message: "Member added to project successfully",
+        member: newMember,
+    })
+);
+
+  })
+
+
+  const getProjectMembers = asyncHandler(async(req,res)=>{
+  const projectId=  req.params.projectId;
+
+  const projectMembers = await ProjectMember.find({
+           project:projectId
+  }) 
+
+  if(!projectMembers){
+    throw new ApiError(404,"no member found")
+  }
+
+  return res.status(200).json(
+    new ApiResponse(201,{mesage:"member found successfully",projectMembers})
+  )
+
+
+  })
+
+
+  const updateMemberRole = asyncHandler(async(req,res)=>{
+   const {projectId} = req.params;
+   const {memberId,role} = req.body;
+   const curUserId = req.user.id;
+
+   const project = await Project.findOne({
+    _id:projectId
+   })
+
+   if(!project){
+    throw new ApiError(404,"project not found")
+   }
+
+   if(project.createdBy.toString()!==curUserId.toString()){
+    throw new ApiError(403,"you are not authorized to update member role");
+   }
+
+   const member = await ProjectMember.findOne({
+         user:memberId
+        });
+
+   if(!member){
+    throw new ApiError(404,"project member not found")
+   }
+
+   if(member.project.toString()!==projectId.toString()){
+    throw new ApiError(403,"member not part of project")
+   }
+
+member.role = role;
+await member.save();
+
+return res.status(200).json(
+  new ApiResponse(200,{message:"member role updated"})
+)
+
+  })
+
+  const deleteMember = asyncHandler(async(req,res)=>{
+     
+    const memberId = req.params.memberId;
+    const curLoggedInUser = req.user.id;
+
+    const projectmember = await ProjectMember.findOne({
+      user:memberId
+    })
+
+    if(!projectmember){
+      throw new ApiError(404,"project member not found")
+    }
+
+    const projectId  =  projectmember.project;
+    const project = await Project.findById(projectId);
+    if(!project){
+      throw new ApiError(404,"project not found")
+    }
+
+    if(project.createdBy.toString()!==curLoggedInUser.toString()){
+      throw new ApiError(403,"not authorized to delete member")
+    }
+
+    await ProjectMember.deleteOne({ _id: projectmember._id });
+
+
+    return res.status(200).json(
+      new ApiResponse(200,{message:"member deleted successfully"})
+    )
+
+  })
+
+
+
+
+export {createProject,updateProject,deleteProject,getProjectById,getProjects,
+    addMemberToProject,getProjectMembers,updateMemberRole,deleteMember
+
+}
--- src/routes/project.route.js
+++ src/routes/project.route.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { createProject, deleteProject, getProjectById, getProjects, updateProject } from '../controllers/project.controller.js';
+import { addMemberToProject, createProject, deleteMember, deleteProject, getProjectById, getProjectMembers, getProjects, updateMemberRole, updateProject } from '../controllers/project.controller.js';
 import { validate } from '../middlewares/validator.middleware.js';
 import { projectCreationValidator,projectUpdationValidator } from '../validators/Project.js';
 import  protectRoute  from "../middlewares/protectRoute.js";
@@ -9,7 +9,13 @@ const router = express.Router();
 router.post('/create',projectCreationValidator(),validate,createProject)
 router.post('/update/:projectId',projectUpdationValidator(),validate,protectRoute,updateProject);
 router.get('/delete/:projectId',protectRoute,deleteProject);
+
 router.get('/getProjectById/:projectId',protectRoute,getProjectById);
 router.get('/getProjects',protectRoute,getProjects);
 
+
+router.get('/addMemberToProject/:projectId',protectRoute,addMemberToProject);
+router.get('/getProjectMembers/:projectId',protectRoute,getProjectMembers);
+router.post('/updateMemberRole/:projectId',protectRoute,updateMemberRole)
+router.get('/deleteMember/:memberId',protectRoute,deleteMember)
 export default router
